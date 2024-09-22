38a60ea48f891cb761c1ce1582319fd0d81e6d29
Gao Haojia <2723687563@qq.com>
2024-09-22 19:34:42 +0800

    add unilidar support

--- src/local_bringup/launch/real_robot.launch.py
+++ src/local_bringup/launch/real_robot.launch.py
@@ -2,7 +2,12 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    TimerAction,
+    GroupAction,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
@@ -14,6 +19,9 @@ def generate_launch_description():
 
     declare_robot_id = DeclareLaunchArgument(
         "robot_id", default_value="0", description=""
+    )
+    declare_lidar_type = DeclareLaunchArgument(
+        "lidar_type", default_value="mid360", description=""
     )
     declare_lio_mode = DeclareLaunchArgument(
         "lio_mode", default_value="point_lio", description=""
@@ -29,7 +37,19 @@ def generate_launch_description():
                 "launch",
                 "msg_MID360_launch.py",
             )
-        )
+        ),
+        condition=LaunchConfigurationEquals("lidar_type", "mid360"),
+    )
+
+    start_unilidar_lidar = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("unitree_lidar_ros2"),
+                "launch",
+                "launch.py",
+            )
+        ),
+        condition=LaunchConfigurationEquals("lidar_type", "unilidar"),
     )
 
     try:
@@ -57,24 +77,50 @@ def generate_launch_description():
         )
     )
 
-    start_fast_lio = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory("fast_lio"),
-                "launch",
-                "mapping_mid360.launch.py",
-            )
+    start_fast_lio = GroupAction(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory("fast_lio"),
+                    "launch",
+                    "mapping_mid360.launch.py",
+                )
+            ),
+            condition=LaunchConfigurationEquals("lidar_type", "mid360"),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory("fast_lio"),
+                    "launch",
+                    "mapping_unilidar.launch.py",
+                )
+            ),
+            condition=LaunchConfigurationEquals("lidar_type", "unilidar"),
         ),
         condition=LaunchConfigurationEquals("lio_mode", "fast_lio"),
     )
 
-    start_point_lio = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory("point_lio"),
-                "launch",
-                "mapping_mid360.launch.py",
-            )
+    start_point_lio = GroupAction(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory("point_lio"),
+                    "launch",
+                    "mapping_mid360.launch.py",
+                )
+            ),
+            condition=LaunchConfigurationEquals("lidar_type", "mid360"),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory("point_lio"),
+                    "launch",
+                    "mapping_unilidar.launch.py",
+                )
+            ),
+            condition=LaunchConfigurationEquals("lidar_type", "unilidar"),
         ),
         condition=LaunchConfigurationEquals("lio_mode", "point_lio"),
     )
@@ -108,6 +154,7 @@ def generate_launch_description():
 
     # Add the actions
     ld.add_action(declare_robot_id)
+    ld.add_action(declare_lidar_type)
     ld.add_action(declare_lio_mode)
     ld.add_action(declare_realsense_mode)
 
@@ -116,6 +163,7 @@ def generate_launch_description():
     except:
         pass
     ld.add_action(start_livox_mid360)
+    ld.add_action(start_unilidar_lidar)
     ld.add_action(start_lidar_transform)
     ld.add_action(start_fast_lio)
     ld.add_action(start_point_lio)
